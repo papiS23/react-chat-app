fdd4e90ba2c863470d3559e0704ddb49f7ba9b91
Bartłomiej Papis <bartekpapis03@gmail.com>
2023-03-06 19:13:40 +0100

    sending and retreiving  messages from databese

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const SendMessage = () => {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      alert("Enter valid message");
+      return;
+    }
+    const { uid, displayName, photoUrl } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoUrl,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    setMessage("");
+  };
+
   return (
     <form className="send-message">
       <label htmlFor="messageInput" hidden>
@@ -12,6 +33,8 @@ const SendMessage = () => {
         type="text"
         className="form-input__input"
         placeholder="type message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <button type="submit">Send</button>
     </form>
--- src/firebase.js
+++ src/firebase.js
@@ -23,5 +23,3 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-//TODO: finished here: How to Send and Store Messages in Firebase
